31a3ff39c572dcb73f38f5bd8270ad8f696804ea
Sricharan Andra <sricharanandra7@gmail.com>
2024-03-20 18:18:54 +0530

    site 4 finish

--- fourthApp/src/components/Button.jsx
+++ fourthApp/src/components/Button.jsx
@@ -1,11 +1,13 @@
 
 function Button() {
     const handleClick = (e) => e.target.textContent = "Clicked"
-    const doubleClick  = (e) => e.target.textContent = "Stop Clicking"
+    const doubleClick  = (e) => e.target.style.display = "none"
     
-    return (
+    return (   <>
                 <button onClick={(e) => handleClick(e)} onDoubleClick={(e) => doubleClick(e)}>Click me</button>
+                </>            
             )
+
 }
 
 export default Button;
